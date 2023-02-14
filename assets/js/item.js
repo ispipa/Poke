849d68f3f73a181f3432ded6a0989d50a00225e1
Genesis <genesisplata1@gmail.com>
2023-02-14 13:36:01 +0100

    habilidades

--- assets/js/item.js
+++ assets/js/item.js
@@ -4,13 +4,17 @@ var koopId = baseUrl.substring(baseUrl.lastIndexOf('=') + 1);
 
 const img = document.querySelector('.item__img')
 const h2 = document.querySelector('.item__h2')
-const p = document.querySelector('.item__p')
+const itemAbilities = document.querySelector('.item__abilities')
 
 axios.get("https://pokeapi.co/api/v2/pokemon/"+koopId+"/").then((response) => {
 
     console.log(response.data)
     h2.innerHTML = response.data.forms[0].name;
     img.src = response.data.sprites.front_default;
-    p.innerHTML = response.data.stats.base_stat;
-    
+
+    response.data.abilities.forEach(x => {
+        const ability = x.ability.name;
+        itemAbilities.innerHTML += `<p>${ability}</p>`
+    })
+
 }).catch((error) => {})
