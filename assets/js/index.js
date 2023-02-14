d94a7eb1553fd1131331dceb4e943a00c5e8419d
ispipa <ispipa123@gmail.com>
2023-02-14 09:34:03 +0100

    funtion

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,15 +1,28 @@
 var parent = document.getElementById("parent");
+var pokes = [];
 
 
-const getPokes = () =>{
-
+const getPokes = async () =>{
     for (let i = 1; i <=10; i++) {
-        axios.get("https://pokeapi.co/api/v2/pokemon/"+i+"/").then((response) => {
+        await axios.get("https://pokeapi.co/api/v2/pokemon/"+i+"/").then((response) => {
+        pokemon = response.data;
+        pokes.push(response.data);
+        }).catch((error) => {
+            console.log(error);
+        });
+
+    }
+    printPokes();
+}
+
+const printPokes =  async(name,url) => {
+    console.log(pokes);
+   pokes.map((poke) => {
         parent.innerHTML +=  `
         <div class=card>
-        <img class=card-img-top src="${response.data.sprites.front_default}" alt=Card image cap>
+        <img class=card-img-top src="${poke.sprites.front_default}" alt=Card image cap>
         <div class=card-body>
-            <h5 class=card-title>${response.data.name}</h5>
+            <h5 class=card-title>${poke.name}</h5>
             <p class=card-text>This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
         </div>
         <div class=card-footer>
@@ -17,11 +30,7 @@ const getPokes = () =>{
         </div>
         </div>
         `;
-        }).catch((error) => {
-            console.log(error);
-        });
-
-    }
+    });
 }
 
 getPokes();
